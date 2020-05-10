e8b1b1418f26a095baa225d134177c4ee1b6ca75
Nathan <44456832+thaniel-c@users.noreply.github.com>
2020-05-09 23:45:37 -0400

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL; 
+typedef pair<int, int> pii; 
+typedef pair<LL, LL> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<LL> vl; 
+typedef vector<vl> vvl; 
+LL n;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	
+
+    return 0;
+}
