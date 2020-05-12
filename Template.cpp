3e4c413ea27e4df2b0d29467d5edb0243bb90263
Nathan Coulas <44456832+thaniel-c@users.noreply.github.com>
2020-05-12 02:26:29 -0400

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -9,12 +9,17 @@ typedef vector<int> vi;
 typedef vector<vi> vvi; 
 typedef vector<pii> vii; 
 typedef vector<LL> vl; 
-typedef vector<vl> vvl; 
+typedef vector<vl> vvl;
+typedef queue<int> qi;
+typedef queue<char> qc;
+typedef stack<int> si;
+typedef stack<char> sc;
 
-int main() {
+
+int main(){
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 	
 
-    return 0;
+	return 0;
 }
